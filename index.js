800cc1c2254c2919664f587ffb82bbbcb628aa57
alexander-daniel <alex.vados@gmail.com>
2018-01-25 16:08:11 -0500

    clean up, use promise API from do-wrapper

--- index.js
+++ index.js
@@ -7,49 +7,10 @@ const { prompt } = require('inquirer');
 const DigitalOcean = require('do-wrapper');
 const api = new DigitalOcean(process.env.DO_KEY);
 const hat = require('hat');
-const { promisify } = require('util');
 
-// Create promisified API
-const papi = {};
-
-Object.getOwnPropertyNames(DigitalOcean.prototype).forEach(funcName => {
-  papi[funcName] = promisify(api[funcName].bind(api));
-});
-
-async function createDroplet(name = hat()) {
-  return papi.dropletsCreate({
-    name,
-    region: 'tor1',
-    size: 's-1vcpu-1gb',
-    image: 'ubuntu-16-04-x64',
-    ssh_keys: ['677813']
-  });
-}
-
-async function deleteDroplet(dropletID) {
-  return new Promise((resolve, reject) => {
-    api.dropletsDelete(dropletID, (err, res, body) => {
-      if (err) return reject(err)
-      resolve(body);
-    });
-  });
-};
-
-async function getAllDroplets() {
-  return new Promise((resolve, reject) => {
-    api.dropletsGetAll({}, (err, res, body) => {
-      if (err) return reject(err);
-      resolve(body);
-    });
-  });
-}
-
-async function getDropletByID(dropletID) {
-  return new Promise((resolve, reject) => {
-    api.dropletsGetById(dropletID, (err, res, body) => {
-      if (err) return reject(err);
-      resolve(body);
-    });
+async function wait(delay = 3000) {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay)
   });
 }
 
@@ -57,11 +18,12 @@ async function deleteAllDroplets() {
   let spinner;
 
   try {
-    const { droplets } = await getAllDroplets();
+    const { body } = await api.dropletsGetAll({});
+    const { droplets } = body;
 
     for (let droplet of droplets) {
       spinner = ora(`Deleting: ${droplet.name}`).start();
-      await deleteDroplet(droplet.id);
+      await api.dropletsDelete(droplet.id)
       spinner.succeed();
     }
 
@@ -74,10 +36,41 @@ async function deleteAllDroplets() {
   }
 }
 
-async function wait(delay = 3000) {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay)
+async function createNewDroplet() {
+  const dropletNameQuestion = [
+    { name: 'dropletName', type: 'input', message: 'choose a droplet name' }
+  ];
+  const { dropletName } = await prompt(dropletNameQuestion);
+  let spinner = ora(`Creating: ${dropletName}`).start();
+
+  const { body } = await api.dropletsCreate({
+    name: dropletName || hat(),
+    region: 'tor1',
+    size: 's-1vcpu-1gb',
+    image: 'ubuntu-16-04-x64',
+    ssh_keys: ['677813']
   });
+
+  const { droplet } = body;
+  spinner.succeed();
+
+  let isDeployed = false;
+  let ip;
+
+  spinner = ora(`Booting instance: ${droplet.name}`).start();
+  while (!isDeployed) {
+    const { body } = await api.dropletsGetById(droplet.id);
+    const dropletMeta = body.droplet;
+    if (dropletMeta.networks.v4.length) {
+      isDeployed = true;
+      ip = dropletMeta.networks.v4[0].ip_address;
+    }
+    await wait();
+  }
+
+  spinner.succeed();
+  console.error(`run: ssh root@${ip} # (copied to clipboard)`);
+  clipboardy.writeSync(`ssh root@${ip}`);
 }
 
 async function main() {
@@ -106,31 +99,7 @@ async function main() {
       }
 
       case 'create': {
-        const dropletNameQuestion = [
-          { name: 'dropletName', type: 'input', message: 'choose a droplet name' }
-        ];
-        const { dropletName } = await prompt(dropletNameQuestion);
-        let spinner = ora(`Creating: ${dropletName}`).start();
-        const { body } = await createDroplet(dropletName);
-        const { droplet } = body;
-        spinner.succeed();
-
-        let isDeployed = false;
-        let ip;
-
-        spinner = ora(`Booting instance: ${droplet.name}`).start();
-        while (!isDeployed) {
-          const dropletMeta = await getDropletByID(droplet.id);
-          if (dropletMeta.droplet.networks.v4.length) {
-            isDeployed = true;
-            ip = dropletMeta.droplet.networks.v4[0].ip_address;
-          }
-          await wait();
-        }
-
-        spinner.succeed();
-        console.error(`run: ssh root@${ip} # (copied to clipboard)`);
-        clipboardy.writeSync(`ssh root@${ip}`);
+        await createNewDroplet();
         break;
       }
 
